fe8f3e5560d4cd054aaac5bf90da53f1a0f2b2ca
root <>
2022-09-25 15:43:25 +0000

    Added release ${FORMULA_VERSION}

--- formula/emt-snapshot.rb
+++ formula/emt-snapshot.rb
@@ -3,8 +3,8 @@ require "formula"
 class Emt < Formula
   desc "Enhanced Mule Tools CLI"
   homepage "https://www.enhanced-mule.com"
-  url "https://oss.sonatype.org/content/repositories/snapshots/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.3.0-rc3/enhanced-mule-tools-cli-@version@-dist.tbz2"
-  sha256 ""
+  url "https://oss.sonatype.org/content/repositories/snapshots/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.3.0-rc3-SNAPSHOT/enhanced-mule-tools-cli-@version@-dist.tbz2"
+  sha256 "5ad708b2665afea54f43a8c0bc56f0d5eeb711e43e5b91c55b6a0e177b363970"
   head "https://github.com/enhanced-mule/homebrew-tools.git"
 
   def install
@@ -13,6 +13,6 @@ class Emt < Formula
   end
 
   test do
-    assert_match "1.3.0-rc3", shell_output("#{bin}/emt -V", 2)
+    assert_match "1.3.0-rc3-SNAPSHOT", shell_output("#{bin}/emt -V", 2)
   end
 end
