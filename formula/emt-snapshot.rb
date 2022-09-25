d2edbb9d052e52d19373b903a689600132db676c
root <>
2022-09-25 08:19:15 +0000

    Added release ${FORMULA_VERSION}

--- formula/emt-snapshot.rb
+++ formula/emt-snapshot.rb
@@ -3,7 +3,7 @@ require "formula"
 class Emt < Formula
   desc "Enhanced Mule Tools CLI"
   homepage "https://www.enhanced-mule.com"
-  url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.3.0-rc3/enhanced-mule-tools-cli-@version@-dist.tbz2"
+  url "https://oss.sonatype.org/content/repositories/snapshots/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.3.0-rc3/enhanced-mule-tools-cli-@version@-dist.tbz2"
   sha256 ""
   head "https://github.com/enhanced-mule/homebrew-tools.git"
 
