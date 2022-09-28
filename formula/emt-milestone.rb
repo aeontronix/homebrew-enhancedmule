ca282772c77aa4946e82921f7ea48b6795249f35
root <>
2022-09-28 15:15:51 +0000

    Added release ${FORMULA_VERSION}

--- formula/emt-milestone.rb
+++ formula/emt-milestone.rb
@@ -3,8 +3,8 @@ require "formula"
 class EmtMilestone < Formula
   desc "Enhanced Mule Tools CLI ( Milestone release )"
   homepage "https://www.enhanced-mule.com"
-  url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.3.0-rc7/enhanced-mule-tools-cli-1.3.0-rc7-dist.tbz2"
-  sha256 "6de45bd48145335475ce92986a6a221e0baf5fd2db2e16171583fb6b79cbfc75"
+  url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.3.0-rc8/enhanced-mule-tools-cli-1.3.0-rc8-dist.tbz2"
+  sha256 "ecc59ae7645323c110fd54aca9818ce1cc2f04dccb4c78cd9494bf636294d145"
   head "https://github.com/enhanced-mule/homebrew-tools.git"
 
   def install
@@ -13,6 +13,6 @@ class EmtMilestone < Formula
   end
 
   test do
-    assert_match "1.3.0-rc7", shell_output("#{bin}/emt -V", 2)
+    assert_match "1.3.0-rc8", shell_output("#{bin}/emt -V", 2)
   end
 end
