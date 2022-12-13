67c6400afa218658499e5bd99a3f3f8145f83e69
root <>
2022-12-13 22:00:20 +0000

    Added release 1.4.11

--- formula/emt.rb
+++ formula/emt.rb
@@ -4,7 +4,7 @@ class Emt < Formula
   desc "Enhanced Mule Tools CLI"
   homepage "https://www.enhanced-mule.com"
   url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.4.11/enhanced-mule-tools-cli-1.4.11-dist.tbz2"
-  sha256 "e1e740898678f4c5058ec2c020452c21fc3728c6bf28e211ea45a7a7633020a0"
+  sha256 ""
   head "https://github.com/enhanced-mule/homebrew-tools.git"
 
   def install
