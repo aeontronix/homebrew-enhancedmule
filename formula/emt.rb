3d378e78e3675de851520ccf919a5cd929379da5
root <>
2023-01-23 07:04:48 +0000

    Added release 1.4.24

--- formula/emt.rb
+++ formula/emt.rb
@@ -3,8 +3,8 @@ require "formula"
 class Emt < Formula
   desc "Enhanced Mule Tools CLI"
   homepage "https://www.enhanced-mule.com"
-  url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.4.23/enhanced-mule-tools-cli-1.4.23-dist.tbz2"
-  sha256 "81901067f8809710c76ad8357f3146123a49977bd0fd02176b0e52dec49a85e3"
+  url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.4.24/enhanced-mule-tools-cli-1.4.24-dist.tbz2"
+  sha256 "102a63ea200adf2ef8c621410651a75aa42e6e0ee90d4f125afb00551105bf38"
   head "https://github.com/enhanced-mule/homebrew-tools.git"
 
   def install
@@ -12,6 +12,6 @@ class Emt < Formula
     bin.write_exec_script libexec/"bin/emt"
   end
     test do
-      assert_match "1.4.23", shell_output("#{bin}/emt -V", 2)
+      assert_match "1.4.24", shell_output("#{bin}/emt -V", 2)
     end
 end
